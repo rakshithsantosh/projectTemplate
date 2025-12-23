d428dd485b3933747b700313fef20256aa96323c
rakshith_s <rakshithsantosh@gmail.com>
2025-12-23 20:03:48 +0530

    add express

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,16 @@
-function welcome(name: string) {
-    console.log(`Welcome, ${name}!`)
+import app from './app.js'
+import { Config } from './config/index.js'
+
+const startServer = () => {
+    const PORT = Config.PORT
+    try {
+        app.listen(PORT, () => {
+            console.log(`Server is running on port ${PORT}`)
+        })
+    } catch (error) {
+        console.error('Error starting the server:', error)
+        process.exit(1)
+    }
 }
 
-welcome('user')
+startServer()
